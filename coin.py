944c43c548291b48239c02d2b2ef3014d4ac8fce
atefkaibenothman <abenothm@uci.edu>
2021-02-19 15:21:11 -0800

    setup

--- coin.py
+++ coin.py
@@ -1,1 +1,32 @@
-print("hello world");
+import hmac
+import json
+import hashlib
+import time
+import base64
+import requests
+from requests.auth import AuthBase
+
+class CoinbaseExchangeAuth(AuthBase):
+    def __init__(self, api_key, secret_key, passphrase):
+        self.api_key = api_key
+        self.secret_key = secret_key
+        self.passphrase = passphrase
+
+    def __call__(self, request):
+        timestamp = str(time.time())
+        message = timestamp + request.method
+        hmac_key = base64.b64decode(self.secret_key)
+        signature = hmac.new(hmac_key, message, hashlib.sha256)
+        signature_b64 = signature.digest().encode("base64").rstrip("\n")
+        
+        request.headers.update({
+            "CB-ACCESS-SIGN": signature_b64,
+            "CB-ACCESS-TIMESTAMP": timestamp,
+            "CB-ACCESS-KEY": self.api_key,
+            "CB-ACCESS-PASSPHRASE": self.passphrase,
+            "Content-Type": "application/json"
+        })
+
+        return(request)
+
+print("hello world!")

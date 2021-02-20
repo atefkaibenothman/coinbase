5e12f24729967f3f3554bc379e7d2dc218095a26
atefkaibenothman <abenothm@uci.edu>
2021-02-19 16:53:52 -0800

    setup authentication

--- coin.py
+++ coin.py
@@ -6,6 +6,21 @@ import base64
 import requests
 from requests.auth import AuthBase
 
+# return map of necessary api keys
+def get_keys():
+    keys = {}
+    with open("keys.json", "r", encoding="utf-8") as json_file:
+        data = json.load(json_file)
+        for d in data:
+            keys[d] = data[d]
+    return keys
+
+keys = get_keys()
+API_KEY = keys["key"]
+API_SECRET = keys["secret"]
+API_PASS = keys["pass"]
+
+# setup the request sent to coinbase
 class CoinbaseExchangeAuth(AuthBase):
     def __init__(self, api_key, secret_key, passphrase):
         self.api_key = api_key
@@ -14,19 +29,44 @@ class CoinbaseExchangeAuth(AuthBase):
 
     def __call__(self, request):
         timestamp = str(time.time())
-        message = timestamp + request.method
+        message = timestamp + request.method + request.path_url + (request.body or b'').decode()
         hmac_key = base64.b64decode(self.secret_key)
-        signature = hmac.new(hmac_key, message, hashlib.sha256)
-        signature_b64 = signature.digest().encode("base64").rstrip("\n")
-        
+        signature = hmac.new(hmac_key, message.encode(), hashlib.sha256)
+        signature_b64 = base64.b64encode(signature.digest()).decode()
+
         request.headers.update({
-            "CB-ACCESS-SIGN": signature_b64,
-            "CB-ACCESS-TIMESTAMP": timestamp,
-            "CB-ACCESS-KEY": self.api_key,
-            "CB-ACCESS-PASSPHRASE": self.passphrase,
-            "Content-Type": "application/json"
-        })
+            'CB-ACCESS-SIGN': signature_b64,
+            'CB-ACCESS-TIMESTAMP': timestamp,
+            'CB-ACCESS-KEY': self.api_key,
+            'CB-ACCESS-PASSPHRASE': self.passphrase,
+            'Content-Type': 'application/json'
+            })
+        return request
 
-        return(request)
+# handle get requests
+def get_data(endpoint):
+    api_url = "https://api.pro.coinbase.com/"
+    auth = CoinbaseExchangeAuth(API_KEY, API_SECRET, API_PASS)
+    r = requests.get(api_url + endpoint, auth=auth)
+    return r.json()
 
-print("hello world!")
+# get all account info with a balance: api.pro.coinbase.com/accounts
+def get_all_accounts():
+    data = get_data("accounts")
+    accounts = []
+    for key in data:
+        bal = float(key["balance"])
+        if(bal > 0):
+            accounts.append(key["id"])
+    return accounts
+
+# get single account info: api.pro.coinbase.com/accounts/<account_id>
+def get_account(account_id):
+    data = get_data(f"accounts/{account_id}")
+    print(data)
+    print()
+
+accounts = get_all_accounts()
+for account in accounts:
+    get_account(account)
+
